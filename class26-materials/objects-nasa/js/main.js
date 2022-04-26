9acbe79b9a48e616374e3e38d5d0f3885df00607
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-04-26 15:59:39 -0500

    updating class 26 and 27

--- class26-materials/objects-nasa/js/main.js
+++ class26-materials/objects-nasa/js/main.js
@@ -1,9 +1,27 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-fetch("https://api.nasa.gov/planetary/apod")
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+  //You need to follow the API's rules, so we need to format the date in a way that NASA is expecting. In this case we were already pulling it as YYYY-MM-DD like they wanted
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=e77iifh98OWgnS4cC5oWhAVE1Yfy8MydLLWaLOiI&date=${choice}`
+  
+  
+  fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
-    })
+        document.querySelector('h3').innerText = data.explanation
+        if (data.media_type === 'image'){
+          document.querySelector('img').src = data.hdurl
+        }else if (data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }}
+    )
+   
     .catch(err => {
         console.log(`error ${err}`)
-    })
+    })}
--- class26-solutions/objects-nasa/js/main.js
+++ class26-solutions/objects-nasa/js/main.js
@@ -1,1 +1,23 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+  //You need to follow the API's rules, so we need to format the date in a way that NASA is expecting. In this case we were already pulling it as YYYY-MM-DD like they wanted
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=e77iifh98OWgnS4cC5oWhAVE1Yfy8MydLLWaLOiI&date=${choice}`
+  
+  
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+    })
+   
+    .catch(err => {
+        console.log(`error ${err}`)
+    })}
