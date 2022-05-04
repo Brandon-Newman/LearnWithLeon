a1d9455541d65465a324c6dd8174197a7f369b11
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-05-04 15:46:37 -0500

    Adding class 28 DND project

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -8,10 +8,29 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data)
+        document.querySelector('h2').innerText = data.name
+        document.querySelector('h3').innerText = data.classes[0].name
+        /* my first try worked with the below
+        let subclassList = []
+        for (i=0;i<data.subclasses.length;i++){
+            subclassList.push(data.subclasses[i].name)
+        }
+        document.querySelector('h4').innerText = subclassList
+        */
+       //LEON"S VERSION
+       data.subclasses.forEach( stuffIGetBack => {
+           console.log(stuffIGetBack.name)
+           //create an li
+           const li = document.createElement('li')
+           //add text to li
+           li.textContent = stuffIGetBack.name
+           //append the li to the ul
+           document.querySelector('ul').appendChild(li)
       })
+    })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
+         
