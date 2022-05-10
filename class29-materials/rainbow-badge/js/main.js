8f69b27a59a8a020a9b66d8adb47710a72883fac
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-05-10 15:29:40 -0500

    last of class 29

--- class29-materials/rainbow-badge/js/main.js
+++ class29-materials/rainbow-badge/js/main.js
@@ -16,29 +16,29 @@ function getFetch(){
         pokeStore.push(data.types[0].type.name)
         pokeImg.push(data.sprites.front_shiny)
         
-        fetch(url2)
-        console.log(data)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
+        //fetch(url2)
+        //console.log(data)
+        //.then(res => res.json()) // parse response as JSON
+        //.then(data => {
 
-          pokeStore.push(data.types[0].type.name)
-          pokeImg.push(data.sprites.front_shiny)
+          //pokeStore.push(data.types[0].type.name)
+          //pokeImg.push(data.sprites.front_shiny)
       
-          if((pokeStore[0] === "grass")){
+          //if((pokeStore[0] === "grass")){
             document.querySelector('#pokeImg1').src = pokeImg[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
+            //document.querySelector('#pokeImg2').src = pokeImg[1]
             document.querySelector('h2').innerText = " 2x > "
-          }
+          
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
 
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+     // })
+     // .catch(err => {
+      //    console.log(`error ${err}`)
+     // });
 
 
 
