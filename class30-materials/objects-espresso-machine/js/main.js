7029e800d498214db8a1d39664f040e0c0f7250d
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-05-10 15:29:18 -0500

    updating class 30

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachineMaker {
+    constructor (brand, color, maxTemp, spouts){
+        this.brand = brand,
+        this.color = color,
+        this.tempMax = maxTemp
+        this.numOfSpouts = spouts
+    }
+    makeEspresso(){
+        console.log("psshhhhh...HOT!!!")
+    }
+    steamMilk(){
+        console.log("nice frothy mil")
+    }
+    grindBeans(){
+        console.log('ggrrrrrr...grinding beans')
+    }
+}
+
+let newEspresso = new EspressoMachineMaker('Nespresso', 'Copper', 208, 2)
