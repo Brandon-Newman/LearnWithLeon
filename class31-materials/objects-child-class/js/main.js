5af1b7d3cab5272d05a4627d7e7deb48c890788a
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-05-12 15:46:22 -0500

    adding 32 and updating 31

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,31 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal {
+    constructor (name){
+        this._name = name
+    }
+    //using underscore and 'get function' means that calling the name from _name and not name. If someone sets Variable.name it won't affect the call function
+    get name(){
+        return this._name
+    }
+    speak() {
+        console.log(`${this.name} makes a sound`)
+    }
+}
+
+class Dog extends Animal {
+    constructor (name, breed){
+        super(name)
+        this.breed = breed
+    }
+}
+
+class Cat extends Animal {
+    constructor (name, color){
+        super(name)
+        this.color = color
+    }
+    speak(){
+        console.log('MEOW')
+    }
+}
