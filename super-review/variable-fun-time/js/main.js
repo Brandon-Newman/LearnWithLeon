d7dfd709c5aaab1842f023a3949ba57058e17968
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-07-08 15:52:43 -0500

    super + 37 +38

--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -16,3 +16,11 @@
 //create a variable that holds the h1
 
 //add an event listener to that element that console logs the sum of the two previous variables
+
+function capitalizeIt (string){
+    let array = string.split(" ")
+    let answer = array.map(element => {
+    return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase();})
+    answer = answer.join(' ')
+    console.log(answer)
+  }
